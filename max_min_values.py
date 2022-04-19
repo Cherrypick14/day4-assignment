d7a551af8201c1ece6035a98ffe97a6321f9c08e
Cherrypick14 <cherrypicklet@gmail.com>
2022-04-19 20:39:41 +0300

    max_min_values_in_tuples

---
+++ max_min_values.py
@@ -0,0 +1,13 @@
+#  dipslaying max and min values in a tuple list
+print("Below are the no of fruits and their quantities")
+
+fruits =[("apples",6),("avocados",15),("blueberries",8),("kiwi",4)]
+
+def max_min(fruits):
+    return max(fruits, key=lambda items:items[1])[1], min(fruits, key=lambda items:items[1])[1]
+    
+
+print("The maximum and minimum no of fruits is: ")
+print(max_min(fruits))
+
+
