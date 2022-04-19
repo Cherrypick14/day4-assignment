4a47b36e88dbe7bf47c15135cdaa89b3333c71c3
Cherrypick14 <cherrypicklet@gmail.com>
2022-04-19 20:45:28 +0300

    first_commit

--- max_min_values.py
+++ max_min_values.py
@@ -3,6 +3,8 @@ print("Below are the no of fruits and their quantities")
 
 fruits =[("apples",6),("avocados",15),("blueberries",8),("kiwi",4)]
 
+print(fruits)
+
 def max_min(fruits):
     return max(fruits, key=lambda items:items[1])[1], min(fruits, key=lambda items:items[1])[1]
     
