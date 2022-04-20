8d9aacc68aae47c9f82181050593457bf341fb4b
Cherrypick14 <cherrypicklet@gmail.com>
2022-04-21 00:13:33 +0300

    added a function

--- distance_between_2_cities.py
+++ distance_between_2_cities.py
@@ -4,10 +4,12 @@
 import haversine as hs
 from haversine import Unit
 
-cairo_cods=(31.507098187652723, 33.30583979211274)
-nairobi_cods=(-1.4899033770642174, 36.20623017525199)
+def distance_calculation():
+    cairo_cods=(31.507098187652723, 33.30583979211274)
+    nairobi_cods=(-1.4899033770642174, 36.20623017525199)
+    return hs.haversine(cairo_cods,nairobi_cods, unit= Unit.MILES)
 
-#   calculating distance in miles
+print(distance_calculation())
 
-print(hs.haversine(cairo_cods,nairobi_cods, unit=Unit.MILES))
+
 
